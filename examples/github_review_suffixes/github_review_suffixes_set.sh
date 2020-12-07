92ad04debfa26ea8d72106d18e3b2a4fb2305ab2
Roland Walker <walker@pobox.com>
2020-12-07 07:35:10 -0500

    tweak example emoji

--- examples/github_review_suffixes/github_review_suffixes_set.sh
+++ examples/github_review_suffixes/github_review_suffixes_set.sh
@@ -2,5 +2,5 @@
 
 git config --global branchdates.indicators.non-pr-review-assigned.suffix "⚪"
 git config --global branchdates.indicators.pr-review-approved.suffix "👍"
-git config --global branchdates.indicators.pr-review-commented.suffix "💭"
+git config --global branchdates.indicators.pr-review-commented.suffix "💬"
 git config --global branchdates.indicators.pr-review-changes-requested.suffix "🛑"
